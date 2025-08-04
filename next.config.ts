245338f9536892abd51d126ffc3ca866ca0643ec
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2025-08-04 15:49:34 +0300

    fix: ignore eslint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
